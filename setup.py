f463a7342a0a6f0b96a4e1d637a90e623de19838
Payton Turner <wdc9um@virginia.edu>
2025-03-18 20:21:15 -0400

    updates

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='Demo',
     version='0.1',
     description='A simple demonstration package',
-    url='http://github.com/ontoligent/m09-demo',
-    author='R.C. Alvarado',
-    author_email='ontoligent@gmail.com',
+    url='https://github.com/payton-turner/ds5100-m09',
+    author='Payton Turner',
+    author_email='wdc9um@virginia.edu',
     license='MIT',
     packages=['demo']
 )
